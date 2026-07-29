ec958509e62ca5409e41fa4cbd826456192b47b6
psym88 <psym88@users.noreply.github.com>
2026-07-29 13:35:02 +0200

    Release version 1.1.1

--- ha_energy-period-card.js
+++ ha_energy-period-card.js
@@ -1,6 +1,6 @@
 // HA Energy Period Card
 
-const CARD_VERSION = "1.1.0-beta.5";
+const CARD_VERSION = "1.1.1";
 const NATIVE_SELECTOR_TAG = "hui-energy-period-selector";
 const NATIVE_SELECTOR_TIMEOUT_MS = 10000;
 const SYNC_DELAY_MS = 80;
